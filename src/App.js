f4b7adad6a6a18e7f40fe83ad5676a87792a05e6
priyachimurkar <priyachimurkar20@gmail.com>
2022-02-07 23:47:06 +0530

    dog-gallery-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './compoents/NavBar/NavBar'
+import SearchBar from './compoents/SearchBar/SearchBar';
+import CardList from './compoents/Card/CardList';
+import PopUp from './compoents/PopUp/PopUp';
+import {useEffect,useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const[names,setNames]=useState([]);
+  const[search,setSearch]=useState('');
+  const[isPopupOpen,setPopup]=useState(false);
+  const[clickedDog,setClickedDog]=useState({});
+  useEffect(() => {
+    fetch(`https://dog.ceo/api/breeds/list/all`)
+        .then(response => response.json())
+        .then(data => {
+            let DogNames = Object.keys(data.message);
+             setNames(DogNames);
+            })
+          }, [])
+const handleChange=(search)=>{
+ setSearch(search);
+}
+const filteredName=names.filter((name) => {
+  return name.toLowerCase().includes(search.toLowerCase())
+})
+const handleClickedPopup=(selectedDog)=>{
+  // console.log(selectedDog);
+  setPopup(true);
+  setClickedDog(selectedDog);
+}
+const handleClose=()=>{
+  setPopup(false);
+  setClickedDog({});
 }
 
+return (
+    <>
+   
+    {isPopupOpen? <PopUp names={filteredName} clickedDog={clickedDog}  handleClose={handleClose}/>:null}
+   
+    <NavBar names={names}/>
+    
+    <SearchBar handleChange={handleChange}/>
+    <CardList names={filteredName} handleClickedPopup={handleClickedPopup} />
+   
+    </>
+    );
+}
 export default App;
+
